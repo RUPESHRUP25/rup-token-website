078db125dd40be560129fa79b12eaedf2e97e667
RUPESHRUP25 <rupeshelectronics2003@gmail.com>
2025-04-13 00:10:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-function spin() {
-  const rewards = ["5 RUP", "10 RUP", "20 RUP", "50 RUP", "Try Again"];
-  const selected = rewards[Math.floor(Math.random() * rewards.length)];
-
-  document.getElementById('result').innerText = You won: ${selected};
-
-  if (selected !== "Try Again") {
-    const claimUrl = https://docs.google.com/forms/d/1c7wCT1bU1dqFXz-JdyZDlF8eCUgvNqr4KA3M3kH-42U/viewform?usp=pp_url&entry.1234567890=${selected};
-    const claimBtn = document.getElementById('claimBtn');
-    claimBtn.href = claimUrl;
-    claimBtn.style.display = 'inline-block';
-  } else {
-    document.getElementById('claimBtn').style.display = 'none';
-  }
+function copyTokenAddress() {
+  const tokenAddress = "0xAa93C8282927c384e5C40C8C0fE11a4B860B6074";
+  navigator.clipboard.writeText(tokenAddress).then(() => {
+    alert("Token Address Copied: " + tokenAddress);
+  });
 }
